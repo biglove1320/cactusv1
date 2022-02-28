2ddd1ddee9ea3180ebdcf94bcbecc69e8b3ee31d
Phanuwat <biglove1320@gmail.com>
2022-02-28 22:57:22 +0700

    add commit

---
+++ js/addres.js
@@ -0,0 +1,11 @@
+function addres()
+{
+    let name = prompt("Name :")
+    let call = prompt("Call: ")
+    let address = prompt("Address: ")
+
+    document.getElementById("nameadd").innerText = (name)
+    document.getElementById("calladd").innerText = (call)
+    document.getElementById("addadd").innerText = (address)
+
+}
---
+++ js/photo.js
@@ -0,0 +1,16 @@
+function save()
+{
+    alert("บันทึกเรียบร้อยแล้ว")
+}
+
+function email()
+{
+    let emailyou = prompt("ChangeEmail :")
+    document.getElementById("email").innerText = (emailyou)
+}
+
+function call()
+{
+    let callyou = prompt("ChangeCall : ")
+    document.getElementById("call").innerText = (callyou)
+}
